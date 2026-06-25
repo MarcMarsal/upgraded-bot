08b282774fc5ff96778c6dc9be72580de379b220
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-06-25 12:43:04 +0200

    Update test_okx.js

--- core/test_okx.js
+++ core/test_okx.js
@@ -4,14 +4,14 @@ export async function testTiers() {
   const url = "https://www.okx.com/api/v5/public/position-tiers?instType=SWAP&tdMode=cross&instFamily=BTC-USDT";
 
   try {
-    console.log("Consultant OKX...");
+    //console.log("Consultant OKX...");
     const res = await fetch(url);
     const json = await res.json();
-    console.log("Resposta OKX:");
-    console.log(JSON.stringify(json, null, 2));
+    //console.log("Resposta OKX:");
+    //console.log(JSON.stringify(json, null, 2));
   } catch (err) {
     console.log("ERROR FETCH:", err.message);
   }
 }
 
-testTiers();
+//testTiers();
