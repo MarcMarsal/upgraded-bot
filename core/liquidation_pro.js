d99e3a915d24e77c33d057d2322c8488f5c70ae5
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-06-25 15:31:06 +0200

    Comment out rebuildProTiersForSymbol call
    Comment out the call to rebuildProTiersForSymbol function.

--- core/liquidation_pro.js
+++ core/liquidation_pro.js
@@ -135,7 +135,7 @@ export async function updateProLiquidity(symbol) {
     );
     
     // 🔁 Pipeline PRO complet per aquest symbol
-    await rebuildProTiersForSymbol(symbol);
+    //await rebuildProTiersForSymbol(symbol);
     //await buildClustersForSymbol(symbol);
     //await buildLiquidationMapForSymbol(symbol);
     
