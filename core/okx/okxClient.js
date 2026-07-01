6649ead6ff1f93969cd3d4bfaf026b45dfa7e35c
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-07-01 19:49:27 +0200

    Update okxClient.js

--- core/okx/okxClient.js
+++ core/okx/okxClient.js
@@ -1,7 +1,9 @@
 import crypto from "crypto";
 import axios from "axios";
 
-const TRADING_API_URL = "https://my.okx.com/api/v5/trade/order";
+
+const TRADING_API_URL = "https://www.okx.com/api/v5/trade/order";
+
 
 const API_KEY = process.env.OKX_API_KEY;
 const SECRET_KEY = process.env.OKX_SECRET_KEY;
@@ -107,7 +109,8 @@ export async function okxCancelOrder(instId, ordId) {
     "x-simulated-trading": "1"
   };
 
-  const url = "https://my.okx.com/api/v5/trade/cancel-order";
+  const url = "https://www.okx.com/api/v5/trade/cancel-order";
+
 
   try {
     const res = await axios.post(url, body, { headers });
