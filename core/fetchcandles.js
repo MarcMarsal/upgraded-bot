88bf6984b620406616954a3d75b8923cf80164a9
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-07-27 09:51:47 +0200

    Update fetchcandles.js

--- core/fetchcandles.js
+++ core/fetchcandles.js
@@ -156,9 +156,11 @@ async function fetchWeex(symbol, timeframe) {
     const res = await axios.get(url, {headers: {"User-Agent": "Mozilla/5.0","Accept": "application/json"}});
 
     const data = res.data.data;
-    console.log(res.data);
+    //console.log(res.data);
     if (!data || data.length === 0) return [];
     //console.log(data);
+    const ts = normalizeTimestamp_WEEX(k[0]); // timestamp
+    console.log(ts);
     return data.map(k => {
       const ts = normalizeTimestamp_WEEX(k[0]); // timestamp
       if (!ts) return null;
