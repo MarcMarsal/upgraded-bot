9571e80c1b6d5ba76321814e558a6b5d884a416e
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-07-27 11:09:05 +0200

    Update bot_microimpulsos.js

--- bot_microimpulsos.js
+++ bot_microimpulsos.js
@@ -161,7 +161,8 @@ export async function processSymbol(symbol, timeframe) {
 
   if (!shouldProcess(symbol)) return;
 
-  const candles = await getCandlesFromDB(symbol, timeframe, 120);
+  //const candles = await getCandlesFromDB(symbol, timeframe, 120);
+  const candles = await getCandlesFromDB(symbol, timeframe, 25);
   if (!candles || candles.length < 40) return;
 
   candles.sort((a, b) => a.timestamp - b.timestamp);
