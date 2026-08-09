772485859d1a6dd92f56f55d6d1b4d7334f12a9f
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-08-09 11:52:54 +0200

    Update fetchcandles.js

--- core/fetchcandles.js
+++ core/fetchcandles.js
@@ -128,8 +128,8 @@ async function fetchOKX(symbol, timeframe) {
     const sym = normalizeSymbolFor("OKX", symbol);
     const tf  = normalizeTimeframeFor("OKX", timeframe);
 
-    //const url = `${API_OKX}?instId=${sym}&bar=${tf}&limit=4`;
-    const url = `${API_OKX}?instId=${sym}&bar=${tf}&limit=50`;
+    const url = `${API_OKX}?instId=${sym}&bar=${tf}&limit=4`;
+    //const url = `${API_OKX}?instId=${sym}&bar=${tf}&limit=50`;
     const res = await axios.get(url);
     const data = res.data.data;
 
