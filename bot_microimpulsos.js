a4dd106596e13a5ade0f3981d338e232e91d6123
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-08-18 12:42:56 +0200

    Update TIMEFRAMES_EXECUTE with new values

--- bot_microimpulsos.js
+++ bot_microimpulsos.js
@@ -71,7 +71,8 @@ function applyMicroPulseFilters(candles, candleIndex, atrManual, type, timeframe
 //const TIMEFRAMES = ["1H"];
 //const TIMEFRAMES = ["1H","1H10m"];
 const TIMEFRAMES_DOWNLOAD = ["1H"];
-const TIMEFRAMES_EXECUTE = ["1H", "1H10m"];
+//const TIMEFRAMES_EXECUTE = ["1H", "1H10m"];
+const TIMEFRAMES_EXECUTE = ["1H","1H07m","1H30m","1H37m"];
 
 
 
@@ -80,7 +81,9 @@ const TIMEFRAMES_EXECUTE = ["1H", "1H10m"];
 // -------------------------------------------------------------
 function timeframeToMs(tf) {
   if (tf === "1H") return 60 * 60 * 1000;
-  if (tf === "1H10m") return 60 * 60 * 1000;   // continua sent 60 min
+  if (tf === "1H07m") return 60 * 60 * 1000;   // continua sent 60 min
+  if (tf === "1H30m") return 60 * 60 * 1000;   // continua sent 60 min
+  if (tf === "1H37m") return 60 * 60 * 1000;   // continua sent 60 min
   if (tf === "15m") return 15 * 60 * 1000;
   throw new Error("Timeframe no suportat: " + tf);
 }
