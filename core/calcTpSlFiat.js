c19981f317401c68b38cb837b1e75c548f586e62
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-08-26 17:19:58 +0200

    Update calcTpSlFiat function logic
    Refactor stop loss and take profit calculations for clarity.

--- core/calcTpSlFiat.js
+++ core/calcTpSlFiat.js
@@ -1,24 +1,41 @@
 // fitxer calcTpSlFiat.js
+//SET sl_new = CASE
+//    WHEN type = 'M' THEN entryr_new - (atr * 0.8)
+//    WHEN type = 'E' THEN entryr_new + (atr * 0.8)
+//END;
 
 export function calcTpSlFiat(type, entryR, thirdBody, atr) {
   // SL FIAT
   let sl;
   if (type === "M") {
-    sl = Math.max(
-      entryR - thirdBody * 0.70,
-      entryR * 0.80
-    );
+    //sl = Math.max(
+    //  entryR - thirdBody * 0.70,
+    //  entryR * 0.80
+    //);
+    sl = entryR -(atr * 0.8);
   } else {
-    sl = Math.min(
-      entryR + thirdBody * 0.70,
-      entryR * 1.20
-    );
+    //sl = Math.min(
+    //  entryR + thirdBody * 0.70,
+    //  entryR * 1.20
+    //);
+    sl = entryR + (atr * 0.8);
   }
 
+  //SET tp_new = CASE
+  //  WHEN type = 'M' THEN entryr_new + (atr * 0.6)
+  //  WHEN type = 'E' THEN entryr_new - (atr * 0.6)
+  //END;
   // TP FIAT (ATR * 0.3)
-  const tp = type === "M"
-    ? entryR + atr * 0.3
-    : entryR - atr * 0.3;
+  //const tp = type === "M"
+  //  ? entryR + atr * 0.3
+  //  : entryR - atr * 0.3;
+  let tp;
+  if (type === "M") {
+    tp = entryR + (atr * 0.6);
+  } else {
+    tp = entryR - (atr * 0.6);
+  }
+  
 
   return { tp, sl };
 }
